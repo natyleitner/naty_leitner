bd3d495528c49babf6e4ab5a70525eb246edca91
Natalia Leitnerova <35874540+natyleitner@users.noreply.github.com>
2021-12-19 14:15:36 -0600

    Navbar (#3)
    * navbar functionality
    
    * cleanup
    
    * new pages

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,19 +1,19 @@
 import Head from 'next/head';
 
-const About = () => {
+const Projects = () => {
   return (
     <div className="container">
       <Head>
-        <title>About me</title>
-        <meta name="description" content="About Naty Leitner" />
+        <title>Projects</title>
+        <meta name="description" content="Projects I have worked on" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1>about</h1>
+        <h1>Projects</h1>
       </main>
     </div>
   );
 };
 
-export default About;
+export default Projects;
